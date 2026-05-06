2bb2d7031d0e2f8354984e1da8d1c83e811d6a80
Urvashi Jadav <jadavurvashi07@gmail.com>
2026-05-07 02:38:20 +0530

    Flask + frontend integration completed with notes

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/user',methods = ['GET'])
-
+@app.route('/user',methods = ['POST']) #by default its GET, we are using POST because we want to take data and send response.
 def user():
-    return "hello world"
+    data = request.get_json() #send req to take json data
+    name = data.get('name') #gets name from json format
 
-if __name__ == "__main__": #ledt side variable and right side string so do '' otherwise it will treate __main__ as var.
+    if not name or not name.strip(): #if name is empty or space only, strip removes space
+        response = {
+            "status":"error",
+            "message":"invalid name"
+        } #resonse json format to send to js file
+        return jsonify(response) #send response json format onlt if if condition is true
+    
+    msg = {
+    "status": "success",
+    "message": f"Hello {name}, request received successfully!"
+    } 
+
+
+    return jsonify(msg) #msf json format if its succesfull.
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-
 
