b3b797a6dc7c8351c1dd603973b4b04a33290559
Urvashi Jadav <jadavurvashi07@gmail.com>
2026-05-08 07:51:26 +0530

    Built Flask register and login API routes with validation

--- app.py
+++ app.py
@@ -4,25 +4,104 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/user',methods = ['POST']) #by default its GET
+
+@app.route('/user', methods=['POST'])
 def user():
+
     data = request.get_json()
+
     name = data.get('name')
+    email = data.get('email')
 
     if not name or not name.strip():
         response = {
-            "status":"error",
-            "message":"invalid name"
+            "status": "error",
+            "message": "Invalid Name"
         }
         return jsonify(response)
-    
-    msg = {
-    "status": "success",
-    "message": f"Hello {name}, request received successfully!"
+
+    if not email or not email.strip():
+        response = {
+            "status": "error",
+            "message": "Invalid Email"
+        }
+        return jsonify(response)
+
+    response = {
+        "status": "success",
+        "message": f"Hello {name}, request received successfully!"
     }
 
-    return jsonify(msg)
+    return jsonify(response)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+
+    reqdata = request.get_json()
+
+    name = reqdata.get("name")
+    username = reqdata.get("username")
+    password = reqdata.get("password")
+
+    if not name or not name.strip():
+        response = {
+            "status": "error",
+            "message": "Invalid Name"
+        }
+        return jsonify(response)
+
+    if not username or not username.strip():
+        response = {
+            "status": "error",
+            "message": "Invalid Username"
+        }
+        return jsonify(response)
+
+    if not password or not password.strip():
+        response = {
+            "status": "error",
+            "message": "Invalid Password"
+        }
+        return jsonify(response)
+
+    response = {
+        "status": "success",
+        "message": f"Hello {name}, You're Successfully REGISTERED!!"
+    }
+
+    return jsonify(response)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    reqdata = request.get_json()
+
+    username = reqdata.get("username")
+    password = reqdata.get("password")
+
+    if not username or not username.strip():
+        response = {
+            "status": "error",
+            "message": "Invalid Username"
+        }
+        return jsonify(response)
+
+    if not password or not password.strip():
+        response = {
+            "status": "error",
+            "message": "Invalid Password"
+        }
+        return jsonify(response)
+
+    response = {
+        "status": "success",
+        "message": f"Hello {username}, You're Successfully LOGGED IN!!"
+    }
+
+    return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
